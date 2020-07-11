be37793fcc88f134acd41d9988c390b32c035250
run <gigimugitya0852@gmail.com>
2020-07-11 22:57:43 +0900

    firebaseをmodelから呼んで表示 mapについて

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
                     onPressed: () {
                       Navigator.push(
                         context,
-                        MaterialPageRoute(builder: (context) => BookList()),
+                        MaterialPageRoute(builder: (context) => BookListPage()),
                       );
                     },
                   ),
